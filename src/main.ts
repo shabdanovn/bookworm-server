1327e56442041d9ee811355d4938f7282205cf86
Shabdanov Nursultan Bekturganovich <shabdanov_n@auca.kg>
2022-03-14 17:47:08 +0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import {AppModule} from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 5000
+  await app.listen(PORT, () => console.log('App listens on port: ' + PORT ));
+
+}
+bootstrap();
